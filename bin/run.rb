fadb70db536c185e600c049298b0880d17805103
J.D. Bean <jonathandbean@gmail.com>
2018-03-26 18:00:08 -0400

    starting initial rough skeleton

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,11 @@
-require_relative 'config/environment'
+require_relative '../config/environment'
+
+def run
+  user = prompt_user
+  User.find_by(username: user)
+  options_prompt
+  user.view_user_agenda
+end
+
+welcome
+run
