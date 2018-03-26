8807fdeae923f1bbbfa20891136d1a1cb01ea4c6
J.D. Bean <jonathandbean@gmail.com>
2018-03-26 15:32:40 -0400

    added initial migrations

--- db/migrate/20180326191807_create_calendars.rb
+++ db/migrate/20180326191807_create_calendars.rb
@@ -1,4 +1,9 @@
 class CreateCalendars < ActiveRecord::Migration[5.0]
   def change
+    create_table :calendars do |t|
+      t.string :name
+      t.string :unique_name
+      t.string :description
+    end
   end
 end
--- db/migrate/20180326191813_create_events.rb
+++ db/migrate/20180326191813_create_events.rb
@@ -1,4 +1,13 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
+    create_table :events do |t|
+      t.string :name
+      t.string :description
+      t.string :location
+      t.datetime :start_time
+      t.datetime :end_time
+      t.integer :calendar_id
+      t.integer :user_id #May become owner_id?
+    end
   end
 end
