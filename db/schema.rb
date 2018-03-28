621fa6aad7d7c626c354bb96c0d07bcdf8128305
J.D. Bean <jonathandbean@gmail.com>
2018-03-28 07:43:19 -0400

    adding password to user db and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326191813) do
+ActiveRecord::Schema.define(version: 20180328113908) do
 
   create_table "calendars", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180326191813) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
+    t.string "password"
   end
 
 end
