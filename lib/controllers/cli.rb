922945acdbb56d3bbad9fd3817fa128d4bd18e67
J.D. Bean <jonathandbean@gmail.com>
2018-03-29 16:22:14 -0400

    fixing yes/no confirmation error message

--- lib/controllers/cli.rb
+++ lib/controllers/cli.rb
@@ -1,5 +1,5 @@
 def welcome
- puts "Hi! Welcome to the Agenda Manager CLI!".colorize(:green)
+ puts "Hi! Welcome to the agenda manager CLI!".colorize(:green)
 end
 
 def new_or_login_prompt
@@ -12,13 +12,6 @@ def new_or_login_prompt
   end
 end
 
-def prompt_user
-  users_array = User.all.map { |o| o.username}
-  ask("Select user (press tab key to autocomplete): ".colorize(:yellow), users_array) do |q|
-    q.readline = true
-  end
-end
-
 def auth(user)
   counter = 3
   while counter > 0
@@ -27,6 +20,19 @@ def auth(user)
       return user
     end
     counter -= 1
+  end
+end
+
+def goodbye
+  puts "Thanks for using the Agenda Manager CLI!".colorize(:green)
+  puts "Please come back soon to check your schedule!".colorize(:red)
+  abort
+end
+
+def prompt_user
+  users_array = User.all.map { |o| o.username} # move to method of User model
+  ask("Select user (press tab key to autocomplete): ".colorize(:yellow), users_array) do |q|
+    q.readline = true
   end
 end
 
@@ -68,13 +74,9 @@ def main_menu(user)
       main_menu(user)
     when menu = :"Create Event"
       system "clear"
-        if user.new_event == "Return Event"
-          system "clear"
-          main_menu(user)
-        else
-          user.reload
-        end
-    main_menu(user)
+      user.new_event
+      user.reload
+      main_menu(user)
     when menu = :"Change User"
       confirm_signout(user)
       system "clear"
@@ -100,6 +102,24 @@ def view_event_detail(str, user)
     system "clear"
     view_event_detail(updated_str, user)
   end
+end
+
+def confirm_delete(user)
+  confirm = ask("ARE YOU SURE YOU WANT TO DELETE THIS? ['yes'/'no'] ".colorize(:red)) do |q|
+    q.limit = 1
+    q.validate = /[yn]/i
+    q.responses[:not_valid] = "Invalid entry, please enter yes or no."
+  end
+  main_menu(user) unless confirm.downcase == 'y'
+end
+
+def confirm_signout(user)
+  confirm = ask("ARE YOU SURE YOU WANT TO SIGN OUT [Y/N] ".colorize(:red)) do |q|
+    q.limit = 1
+    q.validate = /[yn]/i
+    q.responses[:not_valid] = "Invalid entry, please enter yes or no."
+  end
+  main_menu(user) unless confirm.downcase == 'y'
 end
 
 def view_calendar_detail(str, user)
@@ -133,21 +153,6 @@ def view_calendar_detail(str, user)
     system "clear"
     updated_str = calendar.calendar_detail_edit(detail)
     view_calendar_detail(updated_str, user)
+
   end
-end
-
-def confirm_delete(user)
-  confirm = ask("ARE YOU SURE YOU WANT TO DELETE THIS? [Y/N] ".colorize(:red)) { |yn| yn.limit = 1, yn.validate = /[yn]/i }
-  main_menu(user) unless confirm.downcase == 'y'
-end
-
-def confirm_signout(user)
-  confirm = ask("ARE YOU SURE YOU WANT TO SIGN OUT [Y/N] ".colorize(:red)) { |yn| yn.limit = 1, yn.validate = /[yn]/i }
-  main_menu(user) unless confirm.downcase == 'y'
-end
-
-def goodbye
-  puts "Thanks for using the Agenda Manager CLI!".colorize(:green)
-  puts "Please come back soon to check your schedule!".colorize(:red)
-  abort
 end
